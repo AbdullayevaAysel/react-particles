615d5e2af3d5217f5d0ba208eb9b228bb4bc65d6
Aysel Abdullayeva <abdulla2001aysel@gmail.com>
2022-08-15 15:18:53 +0400

    commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,88 +1,12 @@
 import React from 'react';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { useCallback } from "react";
-import "../css/style.css";
+import '../css/style.css';
+import Canvas from './Canvas';
 
 function Home() {
-    const particlesInit = useCallback(async (engine) => {
-        console.log(engine);
-        await loadFull(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async (container) => {
-        await console.log(container);
-    }, []);
-
     return (
         <>
             <div className="wrapper">
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={{
-                    fpsLimit: 120,
-                    particles: {
-                        color: {
-                            value: "#ffffff",
-                        },
-                        links: {
-                            color: "#ffffff",
-                            distance: 250,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1,
-                        },
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            directions: "none",
-                            enable: true,
-                            random: false,
-                            speed: 1,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                area: 800,
-                            },
-                            value: 80,
-                        },
-                        opacity: {
-                            value: 0.5,
-                        },
-                        shape: {
-                            type: "circle",
-                        },
-                        size: {
-                            value: { min: 1, max: 5 },
-                        },
-                    },
-                    interactivity: {
-                        events: {
-                          onhover: {
-                            enable: true,
-                            mode: "grab"
-                          },
-                          resize: true
-                        },
-                        modes: {
-                          grab: {
-                            distance: 150,
-                            line_linked: {
-                              opacity: 1
-                            }
-                          },
-                          
-                        }
-                    },
-                    detectRetina: true,
-                }
-            }
-            />
+                <Canvas />
                 <div className="container">
                     <div className="logo">
                         <img src={require("../image/logo.png")} alt="" />
